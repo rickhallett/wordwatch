4d60443b3a8512a3452f6fb197a45b086559ac8d
Rick Hallett <rickhallett@live.co.uk>
2021-05-27 20:43:27 +0100

    basic responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
         <img src={logo} className="App-logo mx-auto mb-3" alt="logo" />
         <h1 className="text-gray-100 text-3xl">WordWatch</h1>
       </header>
-      <main className="flex">
-        <section className="bg-gray-200 ml-6 p-6 w-2/3 h-96">
+      <main className="flex flex-col md:flex-row">
+        <section className="bg-gray-200 ml-6 mr-6 p-6 flex-grow md:w-2/3 h-96">
           <div className="text-6xl text-green-400">
             <h1>Most popular</h1>
           </div>
@@ -33,22 +33,24 @@ function App() {
             <h6>Sixth most popular</h6>
           </div>
         </section>
-        <section className="bg-gray-300 mr-6 p-6 w-1/3 h-96">
-          <div>
-            <p>
-              Information on topic "<span>books</span>"
+        <section className="bg-gray-300 ml-6 md:ml-0 mr-6 mt-6 md:mt-0 p-6 w-auto md:w-1/3 h-96">
+          <div className="text-left text-gray-500 px-5 md:px-2 max-w-xs mx-auto md:max-w-full">
+            <p className="flex flex-row justify-around">
+              Information on topic <span>"books"</span>
             </p>
-            <p>
+            <hr className="mt-3" />
+            <p className="mt-4 flex flex-row justify-around">
               Total mentions: <span>15</span>
             </p>
-            <div>
-              <p>
+            <hr className="mt-3" />
+            <div className="mt-4">
+              <p className="flex flex-row justify-around">
                 Positive mentions: <span className="text-green-400">10</span>
               </p>
-              <p>
+              <p className="flex flex-row justify-around">
                 Neutral mentions: <span className="text-gray-400">5</span>
               </p>
-              <p>
+              <p className="flex flex-row justify-around">
                 Negative mentions: <span className="text-red-400">0</span>
               </p>
             </div>
