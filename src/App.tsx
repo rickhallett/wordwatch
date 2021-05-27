c4503620661779f142635ea946a09dab6e2a151c
Rick Hallett <rickhallett@live.co.uk>
2021-05-27 21:23:42 +0100

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import "./index.css";
 import "./App.css";
-import logo from "./logo.svg";
+
 import topics from "./topics.json";
 
 import { WordCloud } from "./components/WordCloud";
 import { MetaCloud } from "./components/MetaCloud";
+import { Header } from './components/Header';
 
 function App() {
   console.log(topics);
   return (
     <div className="text-center">
-      <header className="bg-gray-400 m-6 p-6 rounded shadow-lg">
-        <img src={logo} className="App-logo mx-auto mb-3" alt="logo" />
-        <h1 className="text-gray-100 text-3xl">WordWatch</h1>
-      </header>
+      <Header />
       <main className="flex flex-col md:flex-row">
         <WordCloud />
         <MetaCloud />
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import logo from "../logo.svg";
+
+export const Header = (): JSX.Element => {
+  return (
+    <header className="bg-gray-400 m-6 p-6 rounded shadow-lg">
+      <img src={logo} className="App-logo mx-auto mb-3" alt="logo" />
+      <h1 className="text-gray-100 text-3xl">WordWatch</h1>
+    </header>
+  );
+}
