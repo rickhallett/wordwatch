d26579e203cae4fc0784ed29797059465d091500
Rick Hallett <rickhallett@live.co.uk>
2021-05-28 07:39:09 +0100

    shuffle topics for random wordcloud layout

--- src/components/WordCloud.tsx
+++ src/components/WordCloud.tsx
@@ -9,24 +9,7 @@ import { WordCloudData, Topic } from "../types";
 
 export const WordCloud = ({ topics }: WordCloudData): JSX.Element => {
   console.log("topics", topics);
-  console.log(
-    "volumes",
-    topics.map((t) => t.volume)
-  );
-  const sentiments = topics.map((t) => t.sentimentScore);
-  console.log("sentiments", sentiments);
-  console.log(
-    "max sentiment",
-    Math.max(...topics.map((t) => t.sentimentScore))
-  );
-  console.log(
-    "max sentiment",
-    Math.min(...topics.map((t) => t.sentimentScore))
-  );
-  console.log(
-    "avg sentiment",
-    sentiments.reduce((a, b) => a + b) / topics.length
-  );
+  topics.sort(() => Math.random() - 0.5);
   return (
     <section className="bg-gray-200 ml-6 mr-6 p-6 flex-grow md:w-2/3 h-96">
       {topics.map((topic: Topic, index: number) => {
