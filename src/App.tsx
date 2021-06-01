2c8f9306f98b1771d772df6bb18307b49b8a4577
Rick Hallett <rickhallett@live.co.uk>
2021-06-01 10:31:56 +0100

    re-implement original App.jsx data fetching logic

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,22 @@ import { MetaCloud } from "./components/MetaCloud";
 import { Header } from "./components/Header";
 import { selectRandomTopic } from "./util/selectRandomTopic";
 
-import raw from "./topics.json";
-
-declare global {
-  interface Window {
-    React1: any;
-    React2: any;
-  }
-}
-
-// Check react-dom and react for instance conflict
-window.React2 = require("react");
-console.log(window.React1 === window.React2); // false.
+import { ApiInterface } from "./util/getTopicData";
 
 const App = (): JSX.Element => {
-  const [topicData, setTopicData] = useState<Topic[]>(raw.topics);
+  // const data: WordCloudData = rawData;
+  const [topicData, setTopicData] = useState<Topic[]>([] as Topic[]);
   const [activeTopic, setActiveTopic] = useState<Topic>({} as Topic);
 
-  setActiveTopic(selectRandomTopic(raw.topics));
+  useEffect(() => {
+    const asyncFetch = async () => {
+      const data = await ApiInterface.getTopicData();
+      setTopicData(data.topics);
+      setActiveTopic(selectRandomTopic(data.topics));
+    };
+
+    asyncFetch();
+  }, [topicData]);
 
   // TODO: what is the React ts type for a mouse click event? React.MouseEvent does not define event.target.innerText)
   const handleWordClick = (event: any, id: string) => {
