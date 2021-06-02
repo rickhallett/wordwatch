78ee607795e4663af8d7239fac130f50a2af59ef
Rick Hallett <rickhallett@live.co.uk>
2021-06-02 07:43:48 +0100

    SixthPopularTopic tests --passing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -15,6 +15,7 @@ import { SecondPopularTopic } from "./components/topics/SecondPopularTopic";
 import { ThirdPopularTopic } from "./components/topics/ThirdPopularTopic";
 import { ForthPopularTopic } from "./components/topics/ForthPopularTopic";
 import { FifthPopularTopic } from "./components/topics/FifthPopularTopic";
+import { SixthPopularTopic } from "./components/topics/SixthPopularTopic";
 
 /**
  * rennder w/o crash ok
@@ -277,6 +278,33 @@ describe("App", () => {
       const clicked = jest.fn();
       const topicElement = render(
         <FifthPopularTopic topic={data.topics[0]} onWordSelect={clicked} />
+      );
+      const topicHeading = topicElement.getByRole("heading");
+      fireEvent.click(topicHeading);
+      expect(clicked).toHaveBeenCalled();
+    });
+  });
+
+  describe("SixthPopularTopic", () => {
+    it("Renders without crashing", () => {
+      render(<SixthPopularTopic topic={{} as Topic} onWordSelect={noop} />);
+    });
+
+    it("Renders the topic label text", async () => {
+      const data = await ApiInterface.getTopicData();
+      const topic = data.topics[0];
+      const topicElement = render(
+        <SixthPopularTopic topic={topic} onWordSelect={noop} />
+      );
+      const topicHeading = topicElement.getByText(topic.label);
+      expect(topicHeading.innerHTML).toEqual(topic.label);
+    });
+
+    it("Handles a click event via props", async () => {
+      const data = await ApiInterface.getTopicData();
+      const clicked = jest.fn();
+      const topicElement = render(
+        <SixthPopularTopic topic={data.topics[0]} onWordSelect={clicked} />
       );
       const topicHeading = topicElement.getByRole("heading");
       fireEvent.click(topicHeading);
